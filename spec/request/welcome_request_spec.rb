41b21f54c18f863b2e1f84cd643cff6c6f9f2966
Natalie Cook <natalie.t.cook@gmail.com>
2018-08-30 13:32:32 +0000

    welcome request specs

--- spec/request/welcome_request_spec.rb
+++ spec/request/welcome_request_spec.rb
@@ -3,10 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :request do
-  describe 'get/index' do
+  describe 'get index' do
     it 'returns the page with a 200 status' do
       get '/welcome/index'
       expect(response.status).to eq 200
     end
+    
+    it 'renders the index template' do
+      get '/welcome/index'
+      expect(response).to render_template("index")
+    end  
   end
+  
+    describe 'get about' do
+    it 'returns the page with a 200 status' do
+      get '/welcome/about'
+      expect(response.status).to eq 200
+    end
+    
+    it 'renders the about template' do
+      get '/welcome/about'
+      expect(response).to render_template("about")
+    end  
+  end
+  
+    describe 'get contact' do
+    it 'returns the page with a 200 status' do
+      get '/welcome/contact'
+      expect(response.status).to eq 200
+    end
+    
+    it 'renders the contact template' do
+      get '/welcome/contact'
+      expect(response).to render_template("contact")
+    end  
+  end
+  
+  
+  
 end
