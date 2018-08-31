1d7022f86465f279fb05feb640d5f5d1f7d171e6
Natalie Cook <natalie.t.cook@gmail.com>
2018-08-31 16:16:21 +0000

    posts controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  
+  resources :posts
+  
   get 'welcome/index'
   get 'welcome/about'
   get 'welcome/contact'
   get 'welcome/faq'
+  get 'about' => 'welcome#about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
