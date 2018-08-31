2ebf68ed60dd6b647c71b928c9e84a2a48fff90a
Natalie Cook <natalie.t.cook@gmail.com>
2018-08-31 18:29:41 +0000

    add initial posts request spec

--- spec/request/posts_request_spec.rb
+++ spec/request/posts_request_spec.rb
@@ -4,30 +4,33 @@ RSpec.describe PostsController, type: :request do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      get '/posts'
+      expect(response.status).to eq 200
     end
+    
+    it "returns posts" do
+      get '/posts'
+    end  
   end
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
