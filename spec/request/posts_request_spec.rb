3fc56f8595e4f8dd82d1a3a89c7e84a6187c23f1
Natalie Cook <natalie.t.cook@gmail.com>
2018-09-01 11:54:05 +0000

    remove deprecated specs

--- spec/request/posts_request_spec.rb
+++ spec/request/posts_request_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'random_data'
 
 RSpec.describe PostsController, type: :request do
 
@@ -15,12 +16,8 @@ RSpec.describe PostsController, type: :request do
       get '/posts'
       expect(response).to render_template(:index)
     end
-    
-    it "assigns [my_post] to @posts" do
-       get '/posts'
-       expect(assigns(:posts)).to eq([my_post])
-     end
   end
+  
   # describe "GET #show" do
   #   it "returns http success" do
   #     get :show
@@ -28,12 +25,28 @@ RSpec.describe PostsController, type: :request do
   #   end
   # end
 
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET new" do
+    it "returns http success" do
+      get '/posts/new'
+      expect(response.status).to eq 200
+    end
+ 
+    it "renders the #new view" do
+      get '/posts/new'
+      expect(response).to render_template :new
+    end
+  end
+ 
+  describe "POST create" do
+    it "increases the number of Post by 1" do
+      expect{ post '/posts', params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
+    end
+
+    it "redirects to the new post" do
+      post '/posts', params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+      expect(response).to redirect_to Post.last
+    end
+  end
 
   # describe "GET #edit" do
   #   it "returns http success" do
