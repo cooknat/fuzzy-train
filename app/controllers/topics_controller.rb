80cf8d8cae7db298db4ffd40f3230c9a3f99233c
cooknat <natalie.t.cook@gmail.com>
2018-10-04 21:43:22 +0100

    create and edit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -11,4 +11,22 @@ class TopicsController < ApplicationController
 		@topic = Topic.new
 	end
 
+	def create
+		@topic = Topic.new
+		@topic.name = params[:topic][:name]
+		@topic.description = params[:topic][:description]
+		@topic.public = params[:topic][:public]
+
+		if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+	end
+
+	def edit
+		@topic = Topic.find(params[:id])
+	end
+
 end
--- spec/request/topics_request_spec.rb
+++ spec/request/topics_request_spec.rb
@@ -40,4 +40,37 @@ RSpec.describe TopicsController, type: :request do
       expect(response).to render_template :new
      end
   end
+
+
+  describe "POST create" do
+    it "increases the number of topics by 1" do
+      expect{ post '/topics/', params: { topic: { name: RandomData.random_sentence, description: RandomData.random_paragraph } } }.to change(Topic,:count).by(1)
+    end 
+
+    it "redirects to the new topic" do
+      post '/topics/', params: { topic: { name: RandomData.random_sentence, description: RandomData.random_paragraph } }
+      expect(response).to redirect_to Topic.last
+    end
+  end
+
+  describe "GET edit" do
+     it "returns http success" do
+       get "/topics/#{topic.id}/edit/"
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #edit view" do
+       get "/topics/#{topic.id}/edit/"
+       expect(response).to render_template :edit
+     end
+ 
+     # it "assigns topic to be updated to @topic" do
+     #   get :edit, params: {id: my_topic.id}
+     #   topic_instance = assigns(:topic)
+ 
+     #   expect(topic_instance.id).to eq my_topic.id
+     #   expect(topic_instance.name).to eq my_topic.name
+     #   expect(topic_instance.description).to eq my_topic.description
+     # end
+   end
 end
