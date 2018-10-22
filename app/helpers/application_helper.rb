08afb60e0f622646187a04ef914aa7d7297a0626
cooknat <natalie.t.cook@gmail.com>
2018-10-22 21:23:05 +0100

    add error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
+
+   def form_group_tag(errors, &block)
+     css_class = 'form-group'
+     css_class << ' has-error' if errors.any?
+     content_tag :div, capture(&block), class: css_class
+   end
 end
